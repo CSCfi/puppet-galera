bc337a18375b244d9c3a5e4a042ead7191ad786a
Antonio J. Delgado <antonio.delgado@csc.fi>
2022-05-12 14:12:23 +0300

    Add config file and requirements

--- manifests/performance.pp
+++ manifests/performance.pp
@@ -7,14 +7,20 @@ class galera::performance(
     ensure  => directory,
     recurse => true,
   }
+  file {'max-open-files.conf':
+    path   => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
+    ensure => present,
+  }
   file_line{'service_section':
     path => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
     line => '[Service]',
+    require => File['max-open-files.conf'],
   }
   file_line{'max_open_files':
     path  => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
     line  => "LimitNOFILE=${mariadb_max_open_files}",
     match => '^LimitNOFILE=',
     after => '\[Service\]',
+    require => File_line['service_section'],
   }
 }
