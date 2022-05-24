4a47fbd7fa14849595e4696391250c72ecff3394
Antonio J. Delgado <antonio.delgado@csc.fi>
2022-05-24 14:31:35 +0300

    Correct style

--- manifests/performance.pp
+++ manifests/performance.pp
@@ -8,19 +8,19 @@ class galera::performance(
     recurse => true,
   }
   file {'max-open-files.conf':
-    path   => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
     ensure => present,
+    path   => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
   }
   file_line{'service_section':
-    path => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
-    line => '[Service]',
+    path    => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
+    line    => '[Service]',
     require => File['max-open-files.conf'],
   }
   file_line{'max_open_files':
-    path  => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
-    line  => "LimitNOFILE=${mariadb_max_open_files}",
-    match => '^LimitNOFILE=',
-    after => '\[Service\]',
+    path    => '/etc/systemd/system/mariadb.service.d/max-open-files.conf',
+    line    => "LimitNOFILE=${mariadb_max_open_files}",
+    match   => '^LimitNOFILE=',
+    after   => '\[Service\]',
     require => File_line['service_section'],
   }
 }
