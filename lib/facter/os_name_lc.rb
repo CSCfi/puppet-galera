8b988b3587aa92571a1d92f5ddbe16a02d47c3d2
Chris Thomas <chris.thomas@csc.fi>
2024-07-30 13:36:26 +0300

    Update per review

--- lib/facter/os_name_lc.rb
+++ lib/facter/os_name_lc.rb
@@ -1,13 +1,13 @@
 Facter.add(:os_name_lc) do
   setcode do
-      bad_os = Facter.value(:os)['name'].downcase
-      case bad_os
-      when /redhat/
+    bad_os = Facter.value(:os)['name'].downcase
+    case bad_os
+    when /redhat/
       'almalinux'
-     when /centos/
-    'centos'
-      else
-       bad_os
+    when /centos/
+      'centos'
+    else
+      bad_os
     end
   end
 end
